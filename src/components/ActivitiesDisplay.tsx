78b702b2591ccdff5283aa7de7eb1bce5815100c
Yuri Pires <yuripiresbr@gmail.com>
2023-07-21 23:16:22 -0300

    feat: created the layout menu

--- src/components/ActivitiesDisplay.tsx
+++ src/components/ActivitiesDisplay.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Layout from "./Layout";
 
 export default function ActivitiesDisplay() {
   return (
     <>
-      
+      <Layout />
     </>
   );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import hospedaLogo from "../assets/hospedaLogo.svg";
+import userIcon from "../assets/userIcon.svg";
+import homeIcon from "../assets/House.svg";
+import calendarIcon from "../assets/CalendarBlank.svg";
+import moneyIcon from "../assets/CurrencyDollar.svg";
+import chartIcon from "../assets/ChartPie.svg";
+import tagIcon from "../assets/Tag.svg";
 
 export default function Layout() {
   return (
-    <div>Layout</div>
-  )
+    <>
+      <header className="bg-[#ECEEF0] h-[6rem]">
+        <div className="flex justify-between mx-[2rem] py-[1.5rem]">
+          <img src={hospedaLogo} alt="Logo da Hospeda Eventos" />
+          <img
+            src={userIcon}
+            alt="Ícone do usuário logado"
+            className="w-[2.5rem] h-[2.5rem]"
+          />
+        </div>
+      </header>
+      <div className="bg-[#F5F7F8] w-[3rem] h-screen flex justify-center">
+        <ul>
+          <li>
+            <img src={homeIcon} alt="" className="mt-[1.75rem]" />
+          </li>
+          <li>
+            <img src={calendarIcon} alt="" className="mt-[2.5rem]" />
+          </li>
+          <li>
+            <img src={moneyIcon} alt="" className="mt-[2.5rem]" />
+          </li>
+          <li>
+            <img src={chartIcon} alt="" className="mt-[2.5rem]" />
+          </li>
+          <li>
+            <img src={tagIcon} alt="" className="mt-[2.5rem]" />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
