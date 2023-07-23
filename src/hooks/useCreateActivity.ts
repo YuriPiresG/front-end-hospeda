f5fefdb5659fa05e097b0a41f4eb4b7147c8b6e3
Yuri Pires <yuripiresbr@gmail.com>
2023-07-23 17:17:12 -0300

    feat: added create form hook

---
+++ src/hooks/useCreateActivity.ts
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { api } from "../lib/api";
+import { Privacy } from "../constants/privacy";
+
+interface CreateActivity {
+  name: string;
+  privacy: Privacy;
+  description: string;
+  cep: string;
+  streetNumber: number;
+  address: string;
+  additionalInfo: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  initialDate: Date;
+  initialHour: Date;
+}
+
+export const useCreateActivity = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (data: CreateActivity) => {
+      const response = await api.post("/activities", data);
+      queryClient.refetchQueries(["activities"]);
+      console.log(response);
+    },
+  });
+};
