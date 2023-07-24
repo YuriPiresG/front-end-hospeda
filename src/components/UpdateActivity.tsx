b161ad3b657547d73d233eb736302087b357a128
Yuri Pires <yuripiresbr@gmail.com>
2023-07-24 11:37:38 -0300

    style: changed the update style

--- src/components/UpdateActivity.tsx
+++ src/components/UpdateActivity.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Activity } from "../hooks/useGetActivities";
 import { useUpdateActivity } from "../hooks/useUpdateActivity";
 import xIcon from "../assets/xIcon.svg";
+import { Privacy } from "../constants/privacy";
+import { useForm } from "react-hook-form";
 
 interface Props {
   activity: Activity;
@@ -10,56 +12,47 @@ interface Props {
 }
 
 export default function UpdateActivity(props: Props) {
-  const [name, setName] = useState(props.activity.name);
-  const [privacy, setPrivacy] = useState(props.activity.privacy);
-  const [description, setDescription] = useState(props.activity.description);
-  const [cep, setCep] = useState(props.activity.cep);
-  const [streetNumber, setStreetNumber] = useState(props.activity.streetNumber);
-  const [address, setAddress] = useState(props.activity.address);
-  const [additionalInfo, setAdditionalInfo] = useState(
-    props.activity.additionalInfo
-  );
-  const [neighborhood, setNeighborhood] = useState(props.activity.neighborhood);
-  const [city, setCity] = useState(props.activity.city);
-  const [state, setState] = useState(props.activity.state);
-  const [initialDate, setInitialDate] = useState(props.activity.initialDate);
-  const [initialHour, setInitialHour] = useState(props.activity.initialHour);
+  const [activityId] = useState<number>(props.activity.id);
+  const [name] = useState<string>(props.activity.name);
+  const [privacy] = useState<Privacy>(props.activity.privacy);
+
+  const [description] = useState<string>(props.activity.description);
+  const [cep] = useState<string>(props.activity.cep);
+  const [streetNumber] = useState<number>(props.activity.streetNumber);
+  const [address] = useState<string>(props.activity.address);
+  const [additionalInfo] = useState<string>(props.activity.additionalInfo);
+  const [neighborhood] = useState<string>(props.activity.neighborhood);
+  const [city] = useState<string>(props.activity.city);
+  const [state] = useState<string>(props.activity.state);
 
   const { mutateAsync, isLoading } = useUpdateActivity();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    await mutateAsync({
-      id: props.activity.id,
-      name,
-      privacy,
-      description,
-      cep,
-      streetNumber,
-      address,
-      additionalInfo,
-      neighborhood,
-      city,
-      state,
-      initialDate,
-      initialHour,
-    });
+  const { register, handleSubmit } = useForm();
+
+  async function handleUpdateActivity(data: any) {
+    data.id = activityId;
+    data.privacy = data.privacy ? "Privado" : "Público";
+    console.log(data);
+    await mutateAsync(data);
     props.close();
-  };
+  }
 
   return (
     <>
       {props.open && (
         <div className="fixed top-0 bottom-0 left-0 right-0 z-50 bg-[#000000c9] flex justify-center items-center overflow-y-auto">
           <div className="bg-white rounded-lg w-[50rem] h-[55rem] p-6 flex flex-col justify-center items-center">
-            <button className="absolute top-4 right-4" onClick={props.close}>
+            <button className="absolute top-10 right-[35rem]" onClick={props.close}>
               <img
                 src={xIcon}
                 alt=""
                 className="w-5 hover:scale-125 transition-transform"
               />
             </button>
-            <form onSubmit={handleSubmit} className="grid grid-cols-2">
+            <form
+              onSubmit={handleSubmit(handleUpdateActivity)}
+              className="grid grid-cols-2 font-roboto"
+            >
               <label
                 htmlFor="name"
                 className="text-[#171d35] text-[1.5rem] mb-[1rem]"
@@ -68,13 +61,12 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="name"
                 id="name"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                placeholder={name}
+                {...register("name")}
               />
-              
+
               <label
                 htmlFor="privacy"
                 className="text-[#171d35] text-[1.5rem] mb-[1rem]"
@@ -82,11 +74,10 @@ export default function UpdateActivity(props: Props) {
                 Privacidade
               </label>
               <select
-                name="privacy"
                 id="privacy"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={privacy}
-                onChange={(event) => setPrivacy(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                defaultValue={privacy}
+                {...register("privacy")}
               >
                 <option value="Público">Público</option>
                 <option value="Privado">Privado</option>
@@ -99,11 +90,10 @@ export default function UpdateActivity(props: Props) {
                 Descrição
               </label>
               <textarea
-                name="description"
                 id="description"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[5rem] text-[1rem] font-bold"
-                value={description}
-                onChange={(event) => setDescription(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[5rem] text-[1rem] pl-4 mt-[0.5rem] resize-none mb-2"
+                placeholder={description}
+                {...register("description")}
               />
 
               <label
@@ -114,11 +104,10 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="cep"
                 id="cep"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={cep}
-                onChange={(event) => setCep(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                placeholder={cep}
+                {...register("cep")}
               />
 
               <label
@@ -129,11 +118,10 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="streetNumber"
                 id="streetNumber"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={streetNumber}
-                onChange={(event) => setStreetNumber(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                placeholder={String(streetNumber)}
+                {...register("streetNumber")}
               />
 
               <label
@@ -144,11 +132,10 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="address"
                 id="address"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={address}
-                onChange={(event) => setAddress(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                placeholder={address}
+                {...register("address")}
               />
 
               <label
@@ -159,11 +146,10 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="additionalInfo"
                 id="additionalInfo"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={additionalInfo}
-                onChange={(event) => setAdditionalInfo(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                placeholder={additionalInfo}
+                {...register("additionalInfo")}
               />
 
               <label
@@ -174,11 +160,10 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="neighborhood"
                 id="neighborhood"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={neighborhood}
-                onChange={(event) => setNeighborhood(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                placeholder={neighborhood}
+                {...register("neighborhood")}
               />
 
               <label
@@ -189,11 +174,10 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="city"
                 id="city"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold"
-                value={city}
-                onChange={(event) => setCity(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem]"
+                placeholder={city}
+                {...register("city")}
               />
 
               <label
@@ -204,11 +188,10 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="text"
-                name="state"
                 id="state"
-                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] font-bold mb-[2rem]"
-                value={state}
-                onChange={(event) => setState(event.target.value)}
+                className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] text-[1rem] pl-4 mt-[0.5rem] mb-[2rem]"
+                placeholder={state}
+                {...register("state")}
               />
 
               <label
@@ -219,11 +202,9 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="date"
-                name="initialDate"
                 id="initialDate"
                 className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] mb-[2rem]"
-                value={initialDate}
-                onChange={(event) => setInitialDate(event.target.value)}
+                {...register("initialDate")}
               />
 
               <label
@@ -234,16 +215,14 @@ export default function UpdateActivity(props: Props) {
               </label>
               <input
                 type="time"
-                name="initialHour"
                 id="initialHour"
                 className="border-[1px] border-[#171d35] rounded-md w-[20rem] h-[2.5rem] mb-[2rem]"
-                value={initialHour}
-                onChange={(event) => setInitialHour(event.target.value)}
+                {...register("initialHour")}
               />
 
               <button
                 type="submit"
-                className="bg-[#FF6D00] rounded-md w-[10rem] h-[2.5rem] text-white text-[1rem] font-bold mt-4 hover:bg-[#5a2702] transition-all hover:scale-125"
+                className="bg-[#FF6D00] rounded-md w-[10rem] h-[2.5rem] text-white text-[1rem] pl-4 mt-[0.5rem] mt-4 hover:bg-[#5a2702] transition-all hover:scale-125"
               >
                 {isLoading ? "Atualizando..." : "Atualizar"}
               </button>
